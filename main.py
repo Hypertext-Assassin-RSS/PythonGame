6f1b5908aa4ecfd62770176100a4385cde813287
rajithsanjaya <rajith_s@epiclanka.net>
2022-12-28 18:14:11 +0530

    set title, frame rate and quit function

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 import pygame
 
+crashed = False
+
 pygame.init()
 
 gameDisplay = pygame.display.set_mode((800, 600))
+pygame.display.set_caption('pygame')
+clock = pygame.time.Clock()
+
+while not crashed:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            crashed = True
+
+        # print(event)
+
+    pygame.display.update()
+    clock.tick(60)
+
+pygame.quit()
+quit()
